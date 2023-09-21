8fdbc9b99ee6ae9fd6129c2ef9eb54ba8614df9d
JustArmaan <singharmaanbrar@gmail.com>
2023-09-20 19:34:13 -0700

    lab 2 changes

---
+++ Lab-2/main.js
@@ -0,0 +1,24 @@
+// Function will be called here
+
+
+
+// Import the function
+
+const getDayOfTheWeek = require("./lab-two").getDayOfTheWeek;
+const specialOffset = require("./lab-two").specialOffset;
+const isLeapYear = require("./lab-two").isLeapYear;
+const makeCalendar = require("./lab-two").makeCalendar;
+const readline = require("readline-sync");
+
+getDayOfTheWeek(1619, "January", 1);
+getDayOfTheWeek(1704, "February", 1);
+getDayOfTheWeek(2004, "August", 16);
+
+const year = readline.question("Enter a Year: ");
+const month = readline.question("Enter a Month: ");
+const day = readline.question("Enter a date: ");
+
+getDayOfTheWeek(year, month, day);
+
+makeCalendar(2022);
+
